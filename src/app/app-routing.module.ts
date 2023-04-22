bdbe97b026485d176c8da0da1230bdf6b719800a
chehine <bensalahchahine5@gmail.com>
2023-04-22 09:07:34 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+import { AddadminComponent } from './adminCRUD/addadmin/addadmin.component';
+import { UpdateadminComponent } from './adminCRUD/updateadmin/updateadmin.component';
+import { ViewadminComponent } from './adminCRUD/viewadmin/viewadmin.component';
+
+
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  { path: 'admins', component: ViewadminComponent },
+  { path: 'add', component: AddadminComponent },
+  { path: 'update/:cin', component: UpdateadminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {HttpClientModule} from '@angular/common/http'
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddadminComponent } from './adminCRUD/addadmin/addadmin.component';
+import { UpdateadminComponent } from './adminCRUD/updateadmin/updateadmin.component';
+import { ViewadminComponent } from './adminCRUD/viewadmin/viewadmin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    AddadminComponent,
+    UpdateadminComponent,
+    ViewadminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
